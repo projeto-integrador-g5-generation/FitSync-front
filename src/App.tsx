b4a07d29bcf649d713c4f3c4e44360d7380a4373
alineanacleto <alineanacleto07@gmail.com>
2025-02-27 16:18:14 -0300

    :sparkles: CRUD Categoria

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-import FormCategoria from "./componentes/categorias/formcategoria/FormCategoria";
-import DeletarCategoria from "./componentes/categorias/deletarcategoria/DeletarCategoria";
-import ConsultarCategorias from "./componentes/categorias/consultarcategorias/ConsultarCategorias";
-
-
 
 function App() {
-
+  
   return (
-    <>
-      <BrowserRouter>
-       
-        <div className="flex flex-col min-h-screen bg-gray-200 ">
-          <div className="flex-grow">
-            <Routes>
-            
-              <Route path="/categorias" element={<ConsultarCategorias />} />
-              <Route path="/cadastrarcategoria" element={<FormCategoria />} />
-              <Route path="/editarcategoria/:id" element={<FormCategoria />} />
-              <Route path="/deletarcategoria/:id" element={<DeletarCategoria />} />
-             
-            </Routes>
-          </div>
-        </div>
-       
-      </BrowserRouter>
+    <>    
     </>
-  );
+  )
 }
 
 export default App
--- src/models/Categoria.ts
+++ src/models/Categoria.ts
@@ -1,4 +1,6 @@
 export default interface Categoria {
     id: number;
-    tipo: string;
+    nome: string;
+    descricao: string;
+   
 }
