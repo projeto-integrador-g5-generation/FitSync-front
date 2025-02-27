dda906a6ea3bc57f72aa651ab0cfd8b0d0e75213
wellerson pinheiros <wellersonpinheiros@outlook.com>
2025-02-27 11:25:26 -0300

    :sparkles: feat: Service criada e metodos CRUD

---
+++ src/service/Service.ts
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
+export const consultar = async (url: string, setDados: Function) => {
+    const resposta = await api.get(url);
+    setDados(resposta.data);
+  };
+  
+  export const cadastrar = async (
+    url: string,
+    dados: object,
+    setDados: Function
+  ) => {
+    const resposta = await api.post(url, dados);
+    setDados(resposta.data);
+  };
+  
+  export const atualizar = async (
+    url: string,
+    dados: object,
+    setDados: Function
+  ) => {
+    const resposta = await api.put(url, dados);
+    setDados(resposta.data);
+  };
+  
+  export const deletar = async (url: string) => {
+    await api.delete(url);
+  };
+  
