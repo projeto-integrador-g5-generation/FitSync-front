efa6faad3cddf95d23dea6f525c70d3f6a0316ce
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-02-28 10:16:52 -0300

    :lipstick: feat: Adicionado responsividade ao CardExercicio

--- src/components/exercicios/cardexercicios/CardExercicios.tsx
+++ src/components/exercicios/cardexercicios/CardExercicios.tsx
@@ -1,90 +1,62 @@
 import { Link } from "react-router-dom";
 import Exercicio from "../../../models/Exercicio";
 
-
-interface CardExercicioProps{
-  exercicio: Exercicio
+interface CardExercicioProps {
+  exercicio: Exercicio;
 }
 
-function CardExercicio({exercicio}: CardExercicioProps) {
+function CardExercicio({ exercicio }: CardExercicioProps) {
   return (
     <>
-      <div
-        id="container-pai"
-        className="border-1 border-gray-300 w-60 h-105 rounded-4xl flex flex-col overflow-hidden drop-shadow-lg text-sm m-auto bg-white shadow-lg hover:scale-102 transition-all items-center"
-      >
-        <div
-          id="Categoria"
-          className="flex items-center justify-center  w-full bg-cyan-700"
-        >
+      <div className="border-1 border-gray-300 flex flex-col drop-shadow-lg text-sm shadow-lg hover:scale-102 transition-all items-center bg-white p-4 rounded-lg gap-2 max-w-96">
+        <div className="flex items-center justify-center w-full bg-cyan-700 p-2 gap-4  rounded-lg" >
           <div>
-          
-	
-          <Link to={`/deletarexercicios/${exercicio.id}`} >  
-            <img
-              src="https://ik.imagekit.io/50n5k5wmb/heartbeat.svg?updatedAt=1740671289798"
-              alt=""
-              className="w-10 p-0.5"
-            /></Link>
-
-
-            
+            <Link to={`/deletarexercicios/${exercicio.id}`}>
+              <img src="https://ik.imagekit.io/50n5k5wmb/heartbeat.svg?updatedAt=1740671289798" />
+            </Link>
           </div>
-          <h2 className="py-2 bold  text-white">{exercicio.categoria?.nome}</h2>
+          <h2 className="bold text-white">{exercicio.categoria?.nome}</h2>
 
-          <div id="" className="p-1">
-            
-            
-          <Link to={`/editarexercicio/${exercicio.id}`}>
-           
-              <img src="https://ik.imagekit.io/50n5k5wmb/pencil.svg?updatedAt=1740673000487" alt="" />
-            
+          <div>
+            <Link to={`/editarexercicio/${exercicio.id}`}>
+              <img src="https://ik.imagekit.io/50n5k5wmb/pencil.svg?updatedAt=1740673000487" />
             </Link>
           </div>
           <div>
-          <Link to={`/deletarexercicio/${exercicio.id}`} >
-            
-            <img src="https://ik.imagekit.io/50n5k5wmb/trash.svg?updatedAt=1740672961060" alt =""/>
-            
-          </Link>
+            <Link to={`/deletarexercicio/${exercicio.id}`}>
+              <img src="https://ik.imagekit.io/50n5k5wmb/trash.svg?updatedAt=1740672961060" />
+            </Link>
           </div>
-
         </div>
 
-        <div id="NomeExercicio" className="flex justify-center items-center p-4">
+        <div className="flex justify-center items-center p-4 w-full">
           <h1 className="bold text-lg">{exercicio.nome}</h1>
         </div>
 
-        <div
-          id="tempo-exercicio-series"
-          className="w-50 flex flex-col bg-cyan-700 text-white "
-        >
-          <div id="quantidadeseries" className="p-2">
+        <div className="flex flex-col bg-cyan-700 text-white w-full  rounded-lg">
+          <div className="p-2 text-md">
             <h1>Treino:</h1>
-
-            <p>tempo (min): {exercicio.tempo}</p>
+            <p>Tempo (min): {exercicio.tempo}</p>
             <p>Séries: {exercicio.series}</p>
             <p>Repetições: {exercicio.repeticoes}</p>
-            <p>descanso: {exercicio.descanso}</p>
-            <p>carga: {exercicio.carga}</p>
+            <p>Descanso: {exercicio.descanso}</p>
+            <p>Carga: {exercicio.carga}</p>
           </div>
         </div>
 
-        <div id="videoficaaqui" className=" bg-cyan-700  m-4 flex w-50  flex-col justify-center items-center">
-                <h3 className="text-white">
-                   execução do exercicio
-                </h3>
+        <div className=" bg-cyan-700 flex flex-col justify-center items-center w-full  rounded-lg">
+          <h3 className="text-white text-lg">Execução do exercicio</h3>
 
-          <div id="video" className="p-4">
-            <video
-              className="rounded-2xl shadow-lg "
-              controls
-              autoPlay
-              loop
-            >
-              <source src={exercicio.video} />
-              
-            </video>
+          <div id="video" className="w-full p-2 min-h-56">
+            <iframe
+              className="w-full h-full"
+              src={exercicio.video}
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              referrerpolicy="strict-origin-when-cross-origin"
+              allowfullscreen
+            ></iframe>
           </div>
         </div>
       </div>
--- src/components/exercicios/listaexercicios/ListaExercicio.tsx
+++ src/components/exercicios/listaexercicios/ListaExercicio.tsx
@@ -36,15 +36,15 @@ function ListaExercicio() {
           wrapperClass="dna-wrapper mx-auto"
         />
       )}
-      <div>
+      <div className="w-full">
         <div className="flex flex-col justify-center w-full">
-        <div className="flex w-full p-4 justify-end items-center text-center">
-          <button className="p-2 bg-cyan-800 text-white shadow-md rounded-md hover:scale-105 transition-all cursor-pointer text-2xl">
-            <Link to={"/cadastrarexercicio"}>Cadastrar Exercício</Link>
-          </button>
-        </div>
-          <div className="container flex flex-col">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="flex w-full p-4 justify-end items-center text-center">
+            <button className="p-2 bg-cyan-800 text-white shadow-md rounded-md hover:scale-105 transition-all cursor-pointer text-2xl">
+              <Link to={"/cadastrarexercicio"}>Cadastrar Exercício</Link>
+            </button>
+          </div>
+          <div className="flex flex-col w-full">
+            <div className="md:grid-cols-2 justify-center w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-6">
               {exercicios.map((exercicio) => (
                 <CardExercicios key={exercicio.id} exercicio={exercicio} />
               ))}
