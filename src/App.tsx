1b761f531e5b179b252c5bb60a90b537dcc494e8
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2025-02-27 23:18:52 -0300

    :bug: fix: resolvendo conflito no App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,3 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Navbar from "./components/navbar/Navbar"
-import Home from "./pages/home/Home"
-import ListarCategorias from "./components/categorias/listarcategorias/ListarCategorias"
-import FormCategoria from "./components/categorias/formcategoria/FormCategoria"
-import DeletarCategoria from "./components/categorias/deletarcategoria/DeletarCategoria"
-import Footer from "./components/footer/Footer"
 
 function App() {
   
