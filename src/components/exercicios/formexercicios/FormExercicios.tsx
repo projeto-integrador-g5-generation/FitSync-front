efad01784bef886a3a91068c195f73ffad95226a
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-02-28 09:54:22 -0300

    :sparkles: feat: Adicionado botão cadastrar exercicio

--- src/components/exercicios/formexercicios/FormExercicios.tsx
+++ src/components/exercicios/formexercicios/FormExercicios.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { ChangeEvent, useEffect, useState } from "react";
 
 import { useNavigate, useParams } from "react-router-dom";
@@ -8,7 +10,6 @@ import Exercicio from "../../../models/Exercicio";
 import { listar, atualizar, cadastrar } from "../../../service/Service";
 
 function FormExercicios() {
-
   const navigate = useNavigate();
 
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -34,94 +35,72 @@ function FormExercicios() {
     }
   }
 
-
-  async function buscarCategoriaPorId(id: string) {
-    try {
-      await listar(`/categoria/${id}`, setCategoria);
-
-      setCategoria(categoria); // Atribui o objeto categoria diretamente
-    } catch (error: any) {
-      alert("Erro ao Buscar Categoria");
-    }
-  }
-
   async function buscarCategorias() {
     try {
-      await listar(`/categoria`, setCategorias)
+      await listar(`/categoria`, setCategorias);
     } catch (error: any) {
-      alert('Erro ao Buscar Categorias')
+      alert("Erro ao Buscar Categorias");
     }
   }
 
   function handleCategoriaChange(e: ChangeEvent<HTMLSelectElement>) {
-		const selectedId = e.target.value
-		if (selectedId) {
-			buscarCategoriaPorId(selectedId)
-		}
-	}
+    const selectedId = e.target.value;
+    if (!selectedId) return;
 
-  // useEffect(() => {
-  //   if (id) {
-  //     buscarCategoriaPorId(id); // Agora passando 'id' como argumento
-  //     buscarExerciciosPorId(id);
-  //   }
-  // }, [id]);
+    // Encontra a categoria no array de categorias carregadas
+    const categoriaSelecionada = categorias.find(
+      (cat) => cat.id.toString() === selectedId
+    );
+
+    if (categoriaSelecionada) {
+      setCategoria(categoriaSelecionada); // Atualiza o estado da categoria
+      setExercicio((prevState) => ({
+        ...prevState,
+        categoria: categoriaSelecionada, // Atualiza o exercício com a nova categoria
+      }));
+    }
+  }
 
   useEffect(() => {
-    buscarCategorias()
+    buscarCategorias();
 
     if (id !== undefined) {
-      buscarExerciciosPorId(id)
+      buscarExerciciosPorId(id);
     }
-  }, [id])
-
-
-  // useEffect(() => {
-  //   setExercicio({
-  //     ...exercicio,
-
-  //   });
-  // }, [exercicio]);
+  }, [id]);
 
   useEffect(() => {
     setExercicio({
       ...exercicio,
       categoria: categoria,
-    })
-  }, [categoria])
-
-  // function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-  //   setExercicio({
-  //     ...exercicio,
-  //     [e.target.name]: e.target.value,
-  //   });
-  // }
+    });
+  }, [categoria]);
 
   function atualizarEstado(e: ChangeEvent<HTMLInputElement>) {
-		const { type, value } = e.target
+    const { type, value } = e.target;
 
-		let tempo: string | number = value
+    let tempo: string | number = value;
 
-		switch (type) {
-			case 'number':
-			case 'range':
-				tempo = value === '' ? '' : parseFloat(Number(value).toFixed(2))
-				break
-			case 'date':
-				tempo = value
-				break
-			default:
-				// Se não for um dos tipos acima, verifica se é um número
-				if (!isNaN(Number(value)) && value !== '') {
-					tempo = parseFloat(Number(value).toFixed(2))
-				}
-		}
+    switch (type) {
+      case "number":
+      case "range":
+        tempo = value === "" ? "" : parseFloat(Number(value).toFixed(2));
+        break;
+      case "date":
+        tempo = value;
+        break;
+      default:
+        // Se não for um dos tipos acima, verifica se é um número
+        if (!isNaN(Number(value)) && value !== "") {
+          tempo = parseFloat(Number(value).toFixed(2));
+        }
+    }
 
-		setExercicio((prevState) => ({
-			...prevState,
-			[e.target.name]: tempo,
-		}))
-	}
+    setExercicio((prevState) => ({
+      ...prevState,
+      [e.target.name]: tempo,
+    }));
+  }
 
   function retornar() {
     navigate("/exercicios");
@@ -153,7 +132,7 @@ function FormExercicios() {
     retornar();
   }
 
-  const categoriaSelecionado = exercicio.categoria?.id !== undefined
+  const categoriaSelecionado = exercicio.categoria?.id !== undefined;
 
   return (
     <div className="container flex flex-col mx-auto items-center">
@@ -162,226 +141,123 @@ function FormExercicios() {
       </h1>
 
       <form className="flex flex-col w-1/2 gap-4" onSubmit={gerarNovoExercicio}>
-        {/* <div className="flex flex-col gap-2">
-          <label htmlFor="nome">Nome do Exercício</label>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="nome">Exercicio</label>
           <input
+            value={exercicio.nome || ""}
+            onChange={atualizarEstado}
             type="text"
-            placeholder="Digite o nome do Exercicio"
+            placeholder="Insira o nome do Exercicio"
             name="nome"
             required
-            className="border-2 border-slate-700 rounded p-2"
-            value={exercicio.nome}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
           />
         </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="tempo">Tempo do Exercício</label>
+
+        <div className="flex flex-col gap-1">
+          <label htmlFor="tempo">Tempo do Exercicio (min)</label>
           <input
+            value={exercicio.tempo || ""}
+            onChange={atualizarEstado}
             type="number"
-            placeholder="Digite o Tempo do Exercicio"
+            step=".01"
+            placeholder="Adicione o tempo do Exercicio"
             name="tempo"
-            className="border-2 border-slate-700 rounded p-2"
-            value={exercicio.tempo}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
+            className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
           />
         </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="series">Séries do Exercicio</label>
-          <input
-            type="number"
-            placeholder="Digite o numero de series do Exercicio"
-            name="series"
-            className="border-2 border-slate-700 rounded p-2"
-            value={exercicio.series}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-          />
+
+        <div className="flex justify-between w-full flex-wrap">
+          <div className="flex flex-col gap-1 w-1/2 pr-1">
+            <label htmlFor="serie">Séries</label>
+            <input
+              value={exercicio.series || ""}
+              onChange={atualizarEstado}
+              type="number"
+              step=".01"
+              placeholder="Séries"
+              name="series"
+              className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 w-1/2 pl-1">
+            <label htmlFor="repeticao">Repetições</label>
+            <input
+              value={exercicio.repeticoes || ""}
+              onChange={atualizarEstado}
+              type="number"
+              step=".01"
+              placeholder="Repetições"
+              name="repeticoes"
+              className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 w-1/2 pr-1">
+            <label htmlFor="peso">Peso (Kg)</label>
+            <input
+              value={exercicio.carga || ""}
+              onChange={atualizarEstado}
+              type="number"
+              step=".01"
+              placeholder="Peso (Kg)"
+              name="carga"
+              className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1 w-1/2 pl-1">
+            <label htmlFor="descanso">Descanso (min)</label>
+            <input
+              value={exercicio.descanso || ""}
+              onChange={atualizarEstado}
+              type="number"
+              step=".01"
+              placeholder="Descanso (min)"
+              name="descanso"
+              className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
+            />
+          </div>
         </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="repeticoes">Repetições</label>
-          <input
-            type="number"
-            placeholder="Digite o numero de repetições"
-            name="repeticoes"
-            className="border-2 border-slate-700 rounded p-2"
-            value={exercicio.repeticoes}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="descanco">Descanso do Exercicio</label>
-          <input
-            type="number"
-            placeholder="Digite o tempo do Exercicio"
-            name="descanso"
-            className="border-2 border-slate-700 rounded p-2"
-            value={exercicio.descanso}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="carga">Carga do Exercicio</label>
-          <input
-            type="number"
-            placeholder="Digite a quantidade de carga do exercicio"
-            name="carga"
-            className="border-2 border-slate-700 rounded p-2"
-            value={exercicio.carga}
-            onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
-          />
-        </div>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="video">Vídeo do Exercício</label>
+
+        <div className="flex flex-col gap-1 ">
+          <label htmlFor="video">video do Exercício</label>
           <input
             type="text"
-            placeholder="Video do exercicio"
+            placeholder="Insira um video"
             name="video"
-            className="border-2 border-slate-700 rounded p-2"
+            className="border-2 border-slate-700 rounded p-1 utral-800"
+            required
             value={exercicio.video}
             onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
           />
-        </div> */}
-        {/* <div className="flex flex-col gap-2">
-          <p>Tema da Categoria</p>
-          <select
-            name="tema"
-            id="tema"
-            className="border p-2 border-slate-800 rounded"
-            onChange={(e) => buscarCategoriaPorId(e.currentTarget.value)}
-          >
-            <option value="" selected disabled>
-              Selecione uma Categoria
-            </option>
+        </div>
 
+        <div className="flex flex-col gap-1">
+          <p>Categoria</p>
+          <select
+            name="categoria"
+            id="categoria"
+            className="border p-2 border-slate-800 rounded focus:outline-none focus:ring-2 focus:ring-zinc-400"
+            onChange={handleCategoriaChange}
+            value={exercicio.categoria?.id || ""}
+          >
+            <option value="" disabled>
+              Selecione um Categoria
+            </option>
             {categorias.map((categoria) => (
               <option key={categoria.id} value={categoria.id}>
-                {categoria.descricao}
+                {categoria.nome}
               </option>
             ))}
           </select>
-        </div> */}
+        </div>
 
-<div className="flex flex-col gap-1">
-					<label htmlFor="nome">Exercicio</label>
-					<input
-						value={exercicio.nome || ''}
-						onChange={atualizarEstado}
-						type="text"
-						placeholder="Insira o nome do Exercicio"
-						name="nome"
-						required
-						className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
-					/>
-				</div>
-
-				<div className="flex flex-col gap-1">
-					<label htmlFor="tempo">Tempo do Exercicio (min)</label>
-					<input
-						value={exercicio.tempo || ''}
-						onChange={atualizarEstado}
-						type="number"
-						step=".01"
-						placeholder="Adicione o tempo do Exercicio"
-						name="tempo"
-						className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
-					/>
-				</div>
-
-				<div className="flex justify-between w-full flex-wrap">
-
-					<div className="flex flex-col gap-1 w-1/2 pr-1">
-						<label htmlFor="serie">Séries</label>
-						<input
-							value={exercicio.series || ''}
-							onChange={atualizarEstado}
-							type="number"
-							step=".01"
-							placeholder="Séries"
-							name="series"
-							className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
-						/>
-					</div>
-
-					<div className="flex flex-col gap-1 w-1/2 pl-1">
-						<label htmlFor="repeticao">Repetições</label>
-						<input
-							value={exercicio.repeticoes || ''}
-							onChange={atualizarEstado}
-							type="number"
-							step=".01"
-							placeholder="Repetições"
-							name="repeticoes"
-							className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
-						/>
-					</div>
-
-					<div className="flex flex-col gap-1 w-1/2 pr-1">
-						<label htmlFor="peso">Peso (Kg)</label>
-						<input
-							value={exercicio.carga || ''}
-							onChange={atualizarEstado}
-							type="number"
-							step=".01"
-							placeholder="Peso (Kg)"
-							name="carga"
-							className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
-						/>
-					</div>
-
-					<div className="flex flex-col gap-1 w-1/2 pl-1">
-						<label htmlFor="descanso">Descanso (min)</label>
-						<input
-							value={exercicio.descanso || ''}
-							onChange={atualizarEstado}
-							type="number"
-							step=".01"
-							placeholder="Descanso (min)"
-							name="descanso"
-							className="border-2 border-slate-700 rounded p-1 focus:outline-none focus:ring-2 focus:ring-zinc-400"
-						/>
-					</div>
-
-				</div>
-
-				<div className="flex flex-col gap-1 ">
-					<label htmlFor="video">video do Exercício</label>
-					<input
-						type="text"
-						placeholder="Insira um video"
-						name="video"
-						className="border-2 border-slate-700 rounded p-1 utral-800"
-						required
-						value={exercicio.video}
-						onChange={(e: ChangeEvent<HTMLInputElement>) =>
-							atualizarEstado(e)
-						}
-					/>
-				</div>
-
-				<div className="flex flex-col gap-1">
-					<p>Categoria</p>
-					<select
-						name="categoria"
-						id="categoria"
-						className="border p-2 border-slate-800 rounded focus:outline-none focus:ring-2 focus:ring-zinc-400"
-						onChange={handleCategoriaChange}
-						value={exercicio.categoria?.id || ''}
-					>
-						<option value="" disabled>
-							Selecione um Categoria
-						</option>
-						{categorias.map((categoria) => (
-							<option key={categoria.id} value={categoria.id}>
-								{categoria.nome}
-							</option>
-						))}
-					</select>
-				</div>
-        
         <button
           type="submit"
-          className="rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800
-                               text-white font-bold w-1/2 mx-auto py-2 flex justify-center"
-                               disabled={!categoriaSelecionado || isLoading}
+          className="rounded disabled:bg-slate-200 bg-indigo-400 hover:bg-indigo-800 text-white font-bold w-1/2 mx-auto py-2 flex justify-center"
+          disabled={!categoriaSelecionado || isLoading}
           // disabled={carregandoCategoria}
         >
           {isLoading ? (
--- src/components/exercicios/listaexercicios/ListaExercicio.tsx
+++ src/components/exercicios/listaexercicios/ListaExercicio.tsx
@@ -1,66 +1,53 @@
-
 import Exercicio from "../../../models/Exercicio";
 import { useEffect, useState } from "react";
 
 import { DNA } from "react-loader-spinner";
 import CardExercicios from "../cardexercicios/CardExercicios";
 import { listar } from "../../../service/Service";
-
-
-
+import { Link } from "react-router-dom";
 
 function ListaExercicio() {
+  const [exercicios, setExercicios] = useState<Exercicio[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    
+  async function buscarExercicios() {
+    try {
+      await listar("/exercicios", setExercicios);
+    } catch (error: any) {
+      console.log("Erro ao listar os Exercicios!");
+    } finally {
+      setIsLoading(false);
+    }
+  }
 
-    const [exercicios, setExercicios] = useState<Exercicio[]>([])
-    const [isLoading, setIsLoading] = useState<boolean>(false)
-
-
-    async function buscarExercicios() {
-       
-
-        try{
-            await listar('/exercicios', setExercicios)
-        }catch(error: any){
-            console.log("Erro ao listar os Exercicios!")
-        }finally {
-          setIsLoading(false);
-        }
-		
-	}
-
-	useEffect(() => {
-	buscarExercicios()
-	}, [exercicios.length])
-
-
+  useEffect(() => {
+    buscarExercicios();
+  }, [exercicios.length]);
 
   return (
     <>
-    {isLoading && (
-            <DNA
-            visible={true}
-            height="200"
-            width="200"
-            ariaLabel="dna-loading"
-            wrapperStyle={{}}
-            wrapperClass="dna-wrapper mx-auto"
+      {isLoading && (
+        <DNA
+          visible={true}
+          height="200"
+          width="200"
+          ariaLabel="dna-loading"
+          wrapperStyle={{}}
+          wrapperClass="dna-wrapper mx-auto"
         />
-        )}
+      )}
       <div>
-        <div className="flex justify-center w-full my-4">
+        <div className="flex flex-col justify-center w-full">
+        <div className="flex w-full p-4 justify-end items-center text-center">
+          <button className="p-2 bg-cyan-800 text-white shadow-md rounded-md hover:scale-105 transition-all cursor-pointer text-2xl">
+            <Link to={"/cadastrarexercicio"}>Cadastrar Exercício</Link>
+          </button>
+        </div>
           <div className="container flex flex-col">
-            <div
-              className="grid grid-cols-1 md:grid-cols-2 
-                                    lg:grid-cols-3 gap-8"
-            >
-             {exercicios.map((exercicio) => (
-							<CardExercicios
-								key={exercicio.id}
-								exercicio={exercicio}
-							/>
-						))}
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+              {exercicios.map((exercicio) => (
+                <CardExercicios key={exercicio.id} exercicio={exercicio} />
+              ))}
             </div>
           </div>
         </div>
